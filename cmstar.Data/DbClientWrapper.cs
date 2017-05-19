5e3121a6c9aa5c717efb6294a733e64c8a9747a7
cmstar <cosmo_sei@hotmail.com>
2017-05-19 14:10:22 +0800

    现在允许在初始化后修改 DbClientWrapper 所封装的 IDbClient 实例了。

--- cmstar.Data/DbClientWrapper.cs
+++ cmstar.Data/DbClientWrapper.cs
@@ -10,103 +10,118 @@ namespace cmstar.Data
     /// </summary>
     public class DbClientWrapper : IDbClient
     {
-        protected readonly IDbClient InternalClient;
+        private IDbClient _client;
 
         /// <summary>
         /// 初始化类型的新实例，并指定被封装的<see cref="IDbClient"/>实例。
         /// </summary>
-        /// <param name="internalClient"><see cref="IDbClient"/>实例。</param>
-        public DbClientWrapper(IDbClient internalClient)
+        /// <param name="client">被封装的<see cref="IDbClient"/>实例。</param>
+        public DbClientWrapper(IDbClient client)
         {
-            ArgAssert.NotNull(internalClient, "client");
-            InternalClient = internalClient;
+            Client = client;
+        }
+
+        /// <summary>
+        /// 获取或设置当前实例所封装的<seealso cref="IDbClient"/>实例。
+        /// </summary>
+        public IDbClient Client
+        {
+            get
+            {
+                return _client;
+            }
+            set
+            {
+                ArgAssert.NotNull(value, "value");
+                _client = value;
+            }
         }
 
         public virtual object Scalar(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.Scalar(sql, parameters, commandType, timeOut);
+            return _client.Scalar(sql, parameters, commandType, timeOut);
         }
 
         public virtual int Execute(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.Execute(sql, parameters, commandType, timeOut);
+            return _client.Execute(sql, parameters, commandType, timeOut);
         }
 
         public virtual void SizedExecute(int expectedSize, string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            InternalClient.SizedExecute(expectedSize, sql, parameters, commandType, timeOut);
+            _client.SizedExecute(expectedSize, sql, parameters, commandType, timeOut);
         }
 
         public virtual DataTable DataTable(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.DataTable(sql, parameters, commandType, timeOut);
+            return _client.DataTable(sql, parameters, commandType, timeOut);
         }
 
         public virtual DataSet DataSet(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.DataSet(sql, parameters, commandType, timeOut);
+            return _client.DataSet(sql, parameters, commandType, timeOut);
         }
 
         public virtual bool Exists(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.Exists(sql, parameters, commandType, timeOut);
+            return _client.Exists(sql, parameters, commandType, timeOut);
         }
 
         public virtual IDataRecord GetRow(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.GetRow(sql, parameters, commandType, timeOut);
+            return _client.GetRow(sql, parameters, commandType, timeOut);
         }
 
         public object[] ItemArray(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.ItemArray(sql, parameters, commandType, timeOut);
+            return _client.ItemArray(sql, parameters, commandType, timeOut);
         }
 
         public virtual T Get<T>(IMapper<T> mapper, string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.Get(mapper, sql, parameters, commandType, timeOut);
+            return _client.Get(mapper, sql, parameters, commandType, timeOut);
         }
 
         public virtual T ForceGet<T>(IMapper<T> mapper, string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.ForceGet(mapper, sql, parameters, commandType, timeOut);
+            return _client.ForceGet(mapper, sql, parameters, commandType, timeOut);
         }
 
         public virtual IList<T> List<T>(IMapper<T> mapper, string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.List(mapper, sql, parameters, commandType, timeOut);
+            return _client.List(mapper, sql, parameters, commandType, timeOut);
         }
 
         public virtual IEnumerable<IDataRecord> Rows(string sql, IEnumerable<DbParameter> parameters = null,
             CommandType commandType = CommandType.Text, int timeOut = 0)
         {
-            return InternalClient.Rows(sql, parameters, commandType, timeOut);
+            return _client.Rows(sql, parameters, commandType, timeOut);
         }
 
         public virtual ITransactionKeeper CreateTransaction()
         {
-            return InternalClient.CreateTransaction();
+            return _client.CreateTransaction();
         }
 
         public virtual DbParameter CreateParameter()
         {
-            return InternalClient.CreateParameter();
+            return _client.CreateParameter();
         }
 
         public virtual string ConnectionString
         {
-            get { return InternalClient.ConnectionString; }
+            get { return _client.ConnectionString; }
         }
     }
 }
